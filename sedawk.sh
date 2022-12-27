73319118d6f63bb596200916bcd7a9f812147da7
Valery Budich <valery.budich@idp.zyfra.com>
2022-12-27 12:44:43 +0300

    Change to pipe

--- sedawk.sh
+++ sedawk.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-FILE=passwd_new
-
-# a. Create copy of passwd file to passwd_new. exclamation Do all modifications on passwd_new file
-cp passwd "$FILE"
-
-# b. Change shell for user saned from /usr/sbin/nologin to /bin/bash using AWK
-awk -i inplace -F: 'BEGIN {OFS=":"} {if ($1=="saned") $7="/bin/bash"; print}' "$FILE"
-
-# c. Change shell for user avahi from /usr/sbin/nologin to /bin/bash using SED
-sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' "$FILE"
-
-# d. Save only 1-st 3-th 5-th 7-th columns of each string based on : delimiter
-awk -i inplace -F: 'BEGIN {OFS=":"} {print $1, $3, $5, $7}' "$FILE"
-
-# e. Remove all lines from file containing word daemon
-sed -i '/daemon/d' "$FILE"
-
-# f. Change shell for all users with even UID to /bin/zsh
-awk -i inplace -F: 'BEGIN {OFS=":"} {if ($2%2==0) $7="/bin/zsh"; print}' "$FILE"
+cat passwd | awk '/saned/ {gsub("/usr/sbin/nologin", "/bin/bash")}1' | sed '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' | awk -F ":" '{print $1 ":" $3 ":" $5 ":" $7}' | sed '/daemon/d' | awk -F ":" '{if ($2 % 2 == 0 ) {$4="/bin/zsh"; print $1 ":" $2 ":" $3 ":" $4} else {print $1 ":" $2 ":" $3 ":" $4}}' > passwd_new
